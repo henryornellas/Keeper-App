33f34cf5dbabcb61b105ec1c17405cc1a84c80e4
Henry <henrygabriellemos@hotmail.com>
2023-03-31 18:27:03 -0300

    Initial App

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOMClient from 'react-dom/client';
+import App from './components/App.jsx';
+
+ReactDOMClient.createRoot(document.getElementById('root')).render(<App />);
+
+
